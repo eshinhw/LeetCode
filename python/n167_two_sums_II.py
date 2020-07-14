a75fffd70209fae1b5871332e5a869a23d50f37c
Eddie Shin <eshinhw@gmail.com>
2020-07-13 20:34:10 -0400

    Updated

---
+++ python/n167_two_sums_II.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+            
+        
+        for i in range(len(numbers)):
+            
+            remainder = target - numbers[i]
+            if remainder in numbers:
+                return [i+1, numbers.index(remainder,i+1)+1]
