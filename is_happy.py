90c6b1d8becaee4d83bf52d72181668029941cce
Eddie Shin <eshinhw@gmail.com>
2020-07-28 23:33:55 -0400

    Updated

---
+++ is_happy.py
@@ -0,0 +1,28 @@
+
+
+def isHappy(n: int) -> bool:
+    
+    seen = []
+        
+    
+    while (n != 1):
+        
+        curr = n
+        total = 0
+        
+        while (curr != 0):
+            
+            total += (curr % 10) * (curr % 10)
+            curr = curr // 10
+        
+        if total in seen:
+            return False
+    
+
+        seen.append(total)
+        n = total
+            
+    return True
+
+print(isHappy(19))
+
