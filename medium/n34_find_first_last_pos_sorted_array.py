bf083ccb69e5b96e833e92515f07904cdb72fedf
Eddie Shin <eshinhw@gmail.com>
2020-08-07 21:38:08 -0400

    Updated

---
+++ medium/n34_find_first_last_pos_sorted_array.py
@@ -0,0 +1,46 @@
+
+
+class Solution:
+    def searchRange(self, nums, target: int):
+        
+        left, right = 0, len(nums) - 1
+        
+        while (left <= right):
+            mid = (left + right) // 2
+            
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                return [self.find_left(nums, target, 0, mid), self.find_right(nums, target, mid, len(nums)-1)]
+        
+        return[-1,-1]
+    
+    def find_left(self, nums, target, left, right):
+        
+        while (left < right):
+            mid = (left + right) // 2
+            
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid-1] < target:
+                return mid
+            else:
+                right = mid - 1
+                
+        return left   
+    
+    def find_right(self, nums, target, left, right):
+        
+        while (left < right):
+            mid = (left + right) // 2
+            
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid+1] > target:
+                return mid
+            else:
+                left = mid + 1
+        
+        return right
