8c9481a3ec0dc15622c450a297ae46ce048cd263
Eddie Shin <eshinhw@gmail.com>
2020-10-24 13:17:46 -0400

    worked on leetcode #1512

--- 1512-number-of-good-pairs/n1512_number_of_good_pairs.py
+++ 1512-number-of-good-pairs/n1512_number_of_good_pairs.py
@@ -2,57 +2,56 @@
 class Solution:
     def numIdenticalPairs(self, nums) -> int:
         
-        sizeMapping = []
-        
-        
-        
-        count = 0
+        """
+        list length    # good pairs     increment
+        []             0                0
+        [1]            0                0
+        [1,2]          1                1
+        [1,2,3]        3                2
+        [1,2,3,4]      6                3
+        [1,2,3,4,5]    10               4
+        ...
+        """
+
+        # creates a list containing length numbers as indices 
+        # and their corresponding number of valid pairs as values
+        size = [0] * (len(nums) + 1)
         increment = 0
-        
-        for i in range(1,len(nums)):
-            
+
+        for i in range(1,len(nums)+1):
+
             if i == 1:
-            
-                sizeMapping.append((i,count))
+                size[i] = 0
             else:
-                sizeMapping.append((i,sizeMapping[i-1][1]+increment))
-            
-            increment += 1        
-        
-        print(sizeMapping)
-        
-        # numIndex = {}
-        
-        # for i in range(len(nums)):
-            
-        #     if str(nums[i]) in numIndex:
-        #         numIndex[str(nums[i])].append(i)
-        #     else:
-        #         numIndex[str(nums[i])] = [i]
-        
-        # print(numIndex)
-        
-        # total = 0
-        
-        # print(numIndex.values())
-        
-        # for indexList in numIndex.values():
-        #     indexList = list(indexList)
-            
-        #     if len(indexList) > 1:
-        #         length = str(len(indexList))
-        #         total = total + sizeMapping[length]
+                size[i] = size[i-1] + increment
 
-                
-        # print(total)
-        
+            increment += 1
+            
+        # collects index information for each element in a given list
 
-                
-            
-            
+        numIndex = {}
+
+        for i in range(len(nums)):
+
+            if nums[i] in numIndex:
+                numIndex[nums[i]].append(i)
+            else:
+                numIndex[nums[i]] = [i]
+
+        
+        # counts the total number of good pairs 
+        total = 0
+
+        for indexList in numIndex.values():
+
+            total = total + size[len(indexList)]
+
+        return total
+
+
+
 
 
 
 s = Solution().numIdenticalPairs([1,1,1,1])
-        
-        
+
