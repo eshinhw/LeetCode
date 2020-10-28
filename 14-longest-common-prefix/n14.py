c601d6c01d7a2c6ad5c8987c6eb29565dad16e93
Eddie Shin <eshinhw@gmail.com>
2020-10-27 23:31:03 -0400

    worked on leetcode #14

--- 14-longest-common-prefix/n14.py
+++ 14-longest-common-prefix/n14.py
@@ -1,12 +1,52 @@
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs):
+        
+        if len(strs) == 0:
+            
+            return ""    
         
         output = ""
         
-        if strs is None:
-            return output
+        # Find the shortest length
+        
+        shortestLen = len(strs[0])
+        
+        for i in range(len(strs)):
+            shortestLen = min(len(strs[i]),shortestLen)
+        
+        print(shortestLen)
+        
+        # Compare each letter through for loop
+        i = 0
+        while (i < shortestLen):
+            
+            char = strs[0][i]
+            
+            for j in range(1,len(strs)):
+                
+                if strs[j][i] != char:
+                    return output
+                
+            output = output + char
+            i += 1
+        
+        return output
+    
+    def longestCommonPrefixFast(self, strs):
+        
+        if not strs:
+            return ''
+        
+        a, b = min(strs), max(strs)
+        
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return a[:i]
+        return a
+    
+print(Solution().longestCommonPrefixFast(["flower","flow","flight"]))
         
         
             
-        
+print(min(["flower","flow","flight"]))
