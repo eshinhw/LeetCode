84740c60487afdbc9bb44af5c78632213582b9b1
Eddie Shin <eshinhw@gmail.com>
2020-11-22 23:29:38 -0500

    working on leetcode 2

--- 2-add-two-numbers/n2_add_two_numbers.py
+++ 2-add-two-numbers/n2_add_two_numbers.py
@@ -10,16 +10,24 @@ class Solution:
         if l1 is None and l2 is None:
             return None
 
-
         output = ListNode(-1)
 
         t1 = l1
         t2 = l2
 
+        carry = 0
+
         while (t1 != None or t2 != None):
 
+            digit_sum = t1.val + t2.val
+
+            if (digit_sum >= 10):
+                carry += 1
+                remainder = digit_sum - 10
+                output.next = ListNode(remainder)
+            else:
+                output.next = ListNode(digit_sum)
 
 
-            output.next = ListNode(t1.val + t2.val)
 
 
