af2d5c671b35471cc61ff2f85305f12384f29a99
Eddie Shin <eshinhw@gmail.com>
2021-05-12 13:17:38 -0400

    working on leetcode

---
+++ 2darray.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+
+        self.matrix = matrix
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+
+        total_sum = 0
+
+        for i in range(row1, row2+1):
+            for j in range(col1, col2+1):
+                total_sum += self.matrix[i][j]
+
+        return total_sum
+
+matrix = [[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]
+obj = NumMatrix(matrix)
+param_1 = obj.sumRegion(0,0,1,1)
+
