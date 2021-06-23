06b8ca39891678a195d90ac84d7d7be9990cafbb
Eddie Shin <eshinhw@gmail.com>
2021-06-23 14:29:45 -0400

    modified and removed unnecessary files

--- 2darray.py
+++ 2darray.py
@@ -5,6 +5,16 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
 
         self.matrix = matrix
+        col = len(matrix[0])
+        row = len(matrix)
+        self.partial_sum = [[0 for _ in range(col)] for _ in range(row)]
+        for i in range(row):
+            for j in range(col):
+                if j == 0:
+                    self.partial_sum[i][j] = matrix[i][j]
+                else:
+                    self.partial_sum[i][j] = self.partial_sum[i][j-1] + matrix[i][j]
+
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
 
@@ -16,7 +26,7 @@ class NumMatrix:
 
         return total_sum
 
-matrix = [[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
 obj = NumMatrix(matrix)
 param_1 = obj.sumRegion(0,0,1,1)
 
