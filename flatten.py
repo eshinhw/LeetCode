01353235338a65677b8f7bcc111b6c92fa46b59f
Eddie Shin <eshinhw@gmail.com>
2021-06-23 14:54:33 -0400

    updated

---
+++ flatten.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        def _preorder(root):
+            print(root.val)
+            _preorder(root.left)
+            _preorder(root.right)
+        
